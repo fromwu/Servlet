6dd1619af01d56662b0f79a92d7e0e217b82a68c
wulijun <wulijun@walkclass.com>
2019-02-20 11:34:55 +0800

    获取header

---
+++ servletdemo/src/com/yiibai/HeaderServlet.java
@@ -0,0 +1,54 @@
+package com.yiibai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HeaderServlet
+ */
+@WebServlet("/HeaderServlet")
+public class HeaderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HeaderServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+        // out.println("HTTP headers sent by your client:<br>");
+        Enumeration enums = request.getHeaderNames();
+        while (enums.hasMoreElements()) {
+            String headerName = (String) enums.nextElement();
+            String headerValue = request.getHeader(headerName);
+            out.print("<b>" + headerName + "</b>: ");
+            out.println(headerValue + "<br/>");
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
